0684dd4fea1869d1f5376c912cb8ec1a52bc0a8e
Zehan Khan <zkhan@hootboard.com>
2024-01-03 01:31:06 +0530

    feat: :sparkles: create vue nuxt app and config the setup for monorepo

---
+++ apps/nuxt-app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-10 text-center">
+    <h1 class="text-4xl">Nuxt.js App</h1>
+    <ui-button />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Button } from "vue-ui";
+
+export default Vue.extend({
+  components: { "ui-button": Button },
+  name: "IndexPage",
+});
+</script>
---
+++ packages/vue-ui/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="button" @click="handleClick">Click Me</button>
+  </template>
+  
+  <script lang="ts">
+  import Vue from "vue";
+  
+  export default Vue.extend({
+    methods: {
+      handleClick: () => {
+        alert("Woooha!");
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .button {
+    background-color: #3498db;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+  
+  .button:hover {
+    background-color: #2980b9;
+  }
+  </style>
+  
